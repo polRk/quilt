7983fb8608a951bac93b0ac63c15ca2024f0878b
Ismail Syed <isyed867@gmail.com>
2019-09-11 01:22:29 -0400

    Remove some ts-ignores

--- packages/jest-mock-apollo/src/graphqlClient.ts
+++ packages/jest-mock-apollo/src/graphqlClient.ts
@@ -66,7 +66,6 @@ export default function configureClient({
     });
 
     const client = new ApolloClient({
-      // @ts-ignore
       link: memoryLink.concat(mockLink),
       cache,
     }) as MockGraphQLClient;
